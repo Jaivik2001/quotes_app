5692e482a365b92f4762286903d15f67c473be23
Jaivik <jaivikmoradiya2001@gmail.com>
2023-02-26 14:22:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:quotes_app/screens/Like.dart';
+import 'package:quotes_app/screens/allquotes.dart';
+import 'package:quotes_app/screens/homescreen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        'AllQuotes': (context) => AllQuotes(),
+        'AllLike': (context) => AllLike(),
+      },
+    ),
+  );
+}
